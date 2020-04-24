f9f82f8d73b22f8b2314ab3cd999844bedb92403
gabrielcarlson6246 <63062198+gabrielcarlson6246@users.noreply.github.com>
2020-04-24 09:53:55 -0600

    Add files via upload

---
+++ Chapter 1/Newclassone.java
@@ -0,0 +1,14 @@
+/*
+Author:Gabriel Carlson
+Date: 4/24/2020
+
+This file is to find the answer to the following expression
+ */
+
+public class Newclassone {
+
+    public static void main(String[] arg) {
+
+        System.out.println((9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5));
+    }
+}
