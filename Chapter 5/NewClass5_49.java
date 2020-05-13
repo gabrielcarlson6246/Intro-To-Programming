9dcd81ed2dc71d700b4177e414f9a0f54a1c030b
gabrielcarlson6246 <63062198+gabrielcarlson6246@users.noreply.github.com>
2020-05-13 11:08:26 -0600

    Add files via upload

---
+++ Chapter 5/NewClass5_49.java
@@ -0,0 +1,42 @@
+/*
+Author: Gabriel Carlson
+Date: 5/12/2020
+
+This assignment is to count how many vowels and consonants with a given string
+ */
+
+import java.util.Scanner;
+
+public class NewClass5_49 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please Enter any string:");
+        String cv = input.nextLine();
+
+        int vowels, consonants;
+        
+        vowels = 0;
+         consonants = 0;
+
+        for (int i = 0; i < cv.length(); i++) {
+
+            if (Character.isLetter(cv.charAt(i))) {
+                if (Character.toUpperCase(cv.charAt(i)) == 'I'
+                        || Character.toUpperCase(cv.charAt(i)) == 'E'
+                        || Character.toUpperCase(cv.charAt(i)) == 'O'
+                        || Character.toUpperCase(cv.charAt(i)) == 'A'
+                        || Character.toUpperCase(cv.charAt(i)) == 'U') {
+                    vowels++;
+                } else {
+                    consonants++;
+                }
+
+            }
+            System.out.println("The amount of vowels in this string:" + vowels + "\n");
+            System.out.println("The amount of consonants in this string:" + consonants + "\n");
+        }
+    }
+}
